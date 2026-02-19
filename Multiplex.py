3cb543a4e4dcf20ab1cf0c47d824ad5501d6d046
Isaiah Carrin <104584679+iCarrin@users.noreply.github.com>
2026-02-18 19:06:28 -0700

    use old snp far primers

--- Multiplex.py
+++ Multiplex.py
@@ -9,7 +9,12 @@ def multiplex_far(close_primers, snp_list, hetero_max = 9):
     
     '''
     all_good_fars = [] 
+    done_snpid = set()
     for close_primer in close_primers: # find each close primer a far primer match
+        if close_primer.snpID in done_snpid:
+            continue
+        else:
+            done_snpid.add(close_primer.snpID)
         primer_start = 0 #if we have to generate more far primers we know where we left off along the string
         while(True):
             possibles, where_we_ended = generate_matching_primers(close_primer, snp_list, primer_start) #start by getting a list of possible close primers
